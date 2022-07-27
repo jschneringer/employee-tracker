4559ca14aeaa8230620f6ae27b9e2d43d68b1022
Jordan Schneringer <schneringerj@gmail.com>
2022-07-26 20:52:08 -0700

    updated schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name) VALUES ("Information Technology");
+INSERT INTO department (name) VALUES ("Marketing");
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Administration");
+
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 100000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Manager", 85000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Marketing Account Executive", 65000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Chief Operating Officer", 150000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Office Manager" 80000, 4);
+
+INSERT INTO employee (first_name, last_name, department_id) VALUES ("Lebron", "James", 4);
+INSERT INTO employee (first_name, last_name, department_id) VALUES ("Russell", "Westbrook", 2);
+INSERT INTO employee (first_name, last_name, department_id) VALUES ("Donovan", "Mitchell", 1);
+INSERT INTO employee (first_name, last_name, department_id) VALUES ("Luka", "Doncic", 3);
+INSERT INTO employee (first_name, last_name, department_id) VALUES ("Trae", "Young", 3);
+INSERT INTO employee (first_name, last_name, department_id) VALUES ("Lance", "Stephenson", 2);
+INSERT INTO employee (first_name, last_name, department_id) VALUES ("James", "Harden", 2);
