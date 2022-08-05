70f5e757d0a8e9a1541a68150f48b87f2f8511af
Jordan Schneringer <schneringerj@gmail.com>
2022-08-04 20:03:54 -0700

    updated seeds and server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,17 @@
-INSERT INTO department (name) VALUES ("Information Technology");
-INSERT INTO department (name) VALUES ("Marketing");
-INSERT INTO department (name) VALUES ("Sales");
-INSERT INTO department (name) VALUES ("Administration");
+INSERT INTO employee (first_name, last_name, role_id) VALUE ("Lebron", "James", 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUE ("Russell", "Westbrook", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUE ("Luka", "Doncic", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUE ("Trae", "Young", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUE ("James", "Harden", 2);
+ 
+INSERT INTO department (id, department_name) VALUE ("500","Information Technology");
+INSERT INTO department (id, department_name) VALUE ("600", "Marketing");
+INSERT INTO department (id, department_name) VALUE ("700", "Sales");
+INSERT INTO department (id, department_name) VALUE ("900", "Administration");
+INSERT INTO department (id, department_name) VALUE ("100", "PR");
 
-INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 100000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Sales Manager", 85000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Marketing Account Executive", 65000, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Chief Operating Officer", 150000, 4);
-INSERT INTO role (title, salary, department_id) VALUES ("Office Manager" 80000, 4);
-
-INSERT INTO employee (first_name, last_name, department_id) VALUES ("Lebron", "James", 4);
-INSERT INTO employee (first_name, last_name, department_id) VALUES ("Russell", "Westbrook", 2);
-INSERT INTO employee (first_name, last_name, department_id) VALUES ("Donovan", "Mitchell", 1);
-INSERT INTO employee (first_name, last_name, department_id) VALUES ("Luka", "Doncic", 3);
-INSERT INTO employee (first_name, last_name, department_id) VALUES ("Trae", "Young", 3);
-INSERT INTO employee (first_name, last_name, department_id) VALUES ("Lance", "Stephenson", 2);
-INSERT INTO employee (first_name, last_name, department_id) VALUES ("James", "Harden", 2);
+INSERT INTO employee_role (id, title, salary, department_id) VALUE ("500", "Software Engineer", 100000, 202);
+INSERT INTO employee_role (id, title, salary, department_id) VALUE ("600", "Sales Manager", 85000, 101);
+INSERT INTO employee_role (id, title, salary, department_id) VALUE ("700", "Marketing Account Executive", 65000, 303);
+INSERT INTO employee_role (id, title, salary, department_id) VALUE ("900", "Chief Operating Officer", 150000, 404);
+INSERT INTO employee_role (id, title, salary, department_id) VALUE ("100", "Office Manager", 80000, 505);
--- server.js
+++ server.js
@@ -116,3 +116,148 @@ const promptUser = () => {
     };
   });
 };
+
+// function to show all departments 
+showDepartments = () => {
+  console.log('Showing all departments...\n');
+  const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
+};
+
+// function to show all roles 
+showRoles = () => {
+  console.log('Showing all roles...\n');
+
+  const sql = `SELECT role.id, role.title, department.name AS department
+               FROM role
+               INNER JOIN department ON role.department_id = department.id`;
+  
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err; 
+    console.table(rows); 
+    promptUser();
+  })
+};
+
+// function to show all employees 
+showEmployees = () => {
+  console.log('Showing all employees...\n'); 
+  const sql = `SELECT employee.id, 
+                      employee.first_name, 
+                      employee.last_name, 
+                      role.title, 
+                      department.name AS department,
+                      role.salary, 
+                      CONCAT (manager.first_name, " ", manager.last_name) AS manager
+               FROM employee
+                      LEFT JOIN role ON employee.role_id = role.id
+                      LEFT JOIN department ON role.department_id = department.id
+                      LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err; 
+    console.table(rows);
+    promptUser();
+  });
+};
+
+// function to add a department 
+addDepartment = () => {
+  inquirer.prompt([
+    {
+      type: 'input', 
+      name: 'addDept',
+      message: "What department do you want to add?",
+      validate: addDept => {
+        if (addDept) {
+            return true;
+        } else {
+            console.log('Please enter a department');
+            return false;
+        }
+      }
+    }
+  ])
+    .then(answer => {
+      const sql = `INSERT INTO department (name)
+                  VALUES (?)`;
+      connection.query(sql, answer.addDept, (err, result) => {
+        if (err) throw err;
+        console.log('Added ' + answer.addDept + " to departments!"); 
+
+        showDepartments();
+    });
+  });
+};
+
+// function to add a role 
+addRole = () => {
+  inquirer.prompt([
+    {
+      type: 'input', 
+      name: 'role',
+      message: "What role do you want to add?",
+      validate: addRole => {
+        if (addRole) {
+            return true;
+        } else {
+            console.log('Please enter a role');
+            return false;
+        }
+      }
+    },
+    {
+      type: 'input', 
+      name: 'salary',
+      message: "What is the salary of this role?",
+      validate: addSalary => {
+        if (isNAN(addSalary)) {
+            return true;
+        } else {
+            console.log('Please enter a salary');
+            return false;
+        }
+      }
+    }
+  ])
+    .then(answer => {
+      const params = [answer.role, answer.salary];
+
+      // grab dept from department table
+      const roleSql = `SELECT name, id FROM department`; 
+
+      connection.promise().query(roleSql, (err, data) => {
+        if (err) throw err; 
+    
+        const dept = data.map(({ name, id }) => ({ name: name, value: id }));
+
+        inquirer.prompt([
+        {
+          type: 'list', 
+          name: 'dept',
+          message: "What department is this role in?",
+          choices: dept
+        }
+        ])
+          .then(deptChoice => {
+            const dept = deptChoice.dept;
+            params.push(dept);
+
+            const sql = `INSERT INTO role (title, salary, department_id)
+                        VALUES (?, ?, ?)`;
+
+            connection.query(sql, params, (err, result) => {
+              if (err) throw err;
+              console.log('Added' + answer.role + " to roles!"); 
+
+              showRoles();
+       });
+     });
+   });
+ });
+};
